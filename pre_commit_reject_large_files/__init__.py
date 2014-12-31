954488752f2c600ef63a6f95d94e726c121297a7
gkisel <gkisel@truecar.com>
2014-12-30 16:56:59 -0800

    Fix install

--- pre_commit_reject_large_files/__init__.py
+++ pre_commit_reject_large_files/__init__.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
-
 import pkg_resources
 
 __version__ = pkg_resources.get_distribution(
